d7f093ab4de4f6910ac4fb960ebdaf447df5e3e3
mfy <2498917371@qq.com>
2023-03-09 23:05:25 +0800

    dev second commit

--- src/main/java/com/mfy/git/TestGit.java
+++ src/main/java/com/mfy/git/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("哈哈哈");
+        System.out.println("dev啦啦啦");
     }
 }
